4ac71cb526da65c0506d1b6559951cccb95ba99a
jluisdeveloper <jluis.developer@gmail.com>
2022-04-20 00:14:55 -0500

    redirects for admins and user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
 
   def user
   end
+
+  def index
+    redirect_to '/admins/dashboard/index' if current_admin
+    redirect_to '/users/dashboard/index' if current_user
+    redirect_to '/users/sign_in' if !current_admin && !current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,5 @@ Rails.application.routes.draw do
   end
 
   root to: 'pages#index'
+  
 end
